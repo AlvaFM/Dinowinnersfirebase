49f7a475b25ae6cb2530b99206ee91665459b353
din0sauro <auroramansilla1208@gmail.com>
2024-10-31 23:23:47 -0300

    Pantalla de carga
    Se logró

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,26 +1,35 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
-  email: string='';
-  password: string='';
+  email: string = '';
+  password: string = '';
+  loading: boolean = false; 
 
   constructor(private authService: AuthService, private router: Router) {}
 
   async login() {
+    this.loading = true; 
     try {
       await this.authService.login(this.email, this.password);
       console.log('Login exitoso');
-      this.router.navigate(['/home']);
-      this.email ='';
-      this.password = ''; 
+
+      setTimeout(() => {
+        this.router.navigate(['/home']);
+        this.email = '';
+        this.password = '';
+        this.loading = false; 
+      }, 3000); 
+
     } catch (error) {
       console.error('Error al iniciar sesión', error);
-    }
+      this.loading = false; 
+    } 
   }
-} 
+}
